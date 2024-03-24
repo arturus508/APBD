e62efbee5d30f9a4a02eca0812caa974c6d9a31a
Artur Nowowiejski <s23181@pjwstk.edu.pl>
2024-03-24 22:14:44 +0100

    Add method to calculate average

--- ApbdProj/Program.cs
+++ ApbdProj/Program.cs
@@ -1,6 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+public static double CalculateAverage(int[] numbers)
+{
+    if (numbers == null || numbers.Length == 0)
+    {
+        throw new ArgumentException("The numbers array cannot be null or empty.", nameof(numbers));
+    }
 
-// com company  
-//modification
+    double sum = numbers.Sum();
+    return sum / numbers.Length;
+}
